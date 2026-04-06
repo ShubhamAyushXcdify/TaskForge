3efe7367f49b7a53774b2a795e6c0241ca449b0c
RAVISHANKAR YADAV <146420561+ravi-8545@users.noreply.github.com>
2026-04-06 20:39:25 +0530

    feat:fix issue

---
+++ LearnTrack.Infrastructure/Services/EmailService.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Mail;
+using Microsoft.Extensions.Configuration;
+
+namespace LearnTrack.Infrastructure.Services;
+
+public class EmailService
+{
+    private readonly IConfiguration _config;
+
+    public EmailService(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public async Task SendEmailAsync(string to, string subject, string body)
+    {
+        var smtpServer = _config["EmailSettings:SmtpServer"];
+        var port = int.Parse(_config["EmailSettings:Port"]!);
+        var senderEmail = _config["EmailSettings:SenderEmail"];
+        var password = _config["EmailSettings:Password"];
+
+        var client = new SmtpClient(smtpServer, port)
+        {
+            Credentials = new NetworkCredential(senderEmail, password),
+            EnableSsl = true
+        };
+
+        var mail = new MailMessage
+        {
+            From = new MailAddress(senderEmail!),
+            Subject = subject,
+            Body = body,
+            IsBodyHtml = true
+        };
+
+        mail.To.Add(to);
+
+        await client.SendMailAsync(mail);
+    }
+}
