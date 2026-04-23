d54d1f939937bd981b1b8818f72729b3c8ad2e63
RAVISHANKAR YADAV <146420561+ravi-8545@users.noreply.github.com>
2026-04-23 11:25:11 +0530

    feat: add Employee and user Dto

---
+++ LearnTrack.Core/DTOs/EmployeeResponseDto.cs
@@ -0,0 +1,19 @@
+﻿namespace LearnTrack.Core.DTOs;
+
+public class EmployeeResponseDto
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+
+    public string Department { get; set; } = string.Empty;
+    public string EmployeeCode { get; set; } = string.Empty;
+
+    public Guid? ManagerId { get; set; }
+
+    public string EmploymentStatus { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; }
+}
