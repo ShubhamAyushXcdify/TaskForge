9fcf6cb35b72e558f1e51ce7d8267bfe37dfc0ad
Swayum Bansal <swayambansal2012@gmail.com>
2026-05-16 14:03:47 +0530

    fix: fix todos and documents table casing via migration

--- LearnTrackAPI/LearnTrack.Core/Entities/Todo.cs
+++ LearnTrackAPI/LearnTrack.Core/Entities/Todo.cs
@@ -1,15 +1,34 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LearnTrack.Core.Entities;
 
+[Table("todos")]
 public class Todo
 {
+    [Key]
+    [Column("id")]
     public Guid Id { get; set; }
+
+    [Column("userid")]
     public Guid UserId { get; set; }
+
+    [Column("title")]
     public string Title { get; set; } = string.Empty;
+
+    [Column("description")]
     public string? Description { get; set; }
+
+    [Column("duedate")]
     public DateTime? DueDate { get; set; }
+
+    [Column("iscompleted")]
     public bool IsCompleted { get; set; } = false;
+
+    [Column("createdat")]
     public DateTime CreatedAt { get; set; }
+
+    [Column("completedat")]
     public DateTime? CompletedAt { get; set; }
 }
